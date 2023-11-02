a4a01262be7222ecb4d95d134a81bd1dec63056f
Sergio Flores <sergioflores.vt@gmail.com>
2023-11-01 23:31:17 -0300

    Creacion de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
+import { HeaderComponent } from './modules/shared/header/header.component';
+import { NavComponent } from './modules/shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
